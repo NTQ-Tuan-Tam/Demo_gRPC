8d8f0f5714824a0028ecb4cd8a3b4d04a1d24aaa
tuan.vu2 <tuan.vu2@ntq-solution.com.vn>
2023-01-09 16:06:15 +0700

    add project

---
+++ GrpcServiceDemo/Services/CustomerService.cs
@@ -0,0 +1,72 @@
+﻿using Grpc.Core;
+using GrpcServiceDemo.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GrpcServiceDemo.Services
+{
+    public class CustomerService : Customer.CustomerBase
+    {
+        private readonly ILogger<CustomerService> _logger;
+
+        public CustomerService(ILogger<CustomerService> logger)
+        {
+            this._logger = logger;
+        }
+        public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+        {
+            CustomerModel ouput = new CustomerModel();
+            if (request.UserId == 1 )
+            {
+                ouput.FistName = "tuan";
+                ouput.LastName = "tam ";
+            }else if (request.UserId == 2 )
+            {
+                ouput.FistName = "tuan 2 ";
+                ouput.LastName = "tam 2 ";
+            }
+            else
+            {
+                ouput.FistName = "fane";
+                ouput.LastName = "thomas";
+            }
+            return Task.FromResult(ouput);
+        }
+        public override async Task GetNewCustomers(NewCustomerRetquest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+        {
+            List<CustomerModel> customers = new List<CustomerModel>
+            {
+                new CustomerModel
+                {
+                    FistName = "tim",
+                    LastName = "cat",
+                    EmailAddrees = " cattim@gmail.com",
+                    Age = 12,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FistName = "chim",
+                    LastName = "co1",
+                    EmailAddrees = " cattim@gmail.com",
+                    Age = 12,
+                    IsAlive = true
+                },
+                new CustomerModel
+                {
+                    FistName = "tim3",
+                    LastName = "cat5",
+                    EmailAddrees = " cattim@gmail.com",
+                    Age = 12,
+                    IsAlive = true
+                },
+            };
+            foreach (var cust in customers)
+            {
+                await responseStream.WriteAsync(cust);
+            }
+        }
+    }
+}
---
+++ grpcCLien1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+
+using GrpcServiceDemo;
+using GrpcServiceDemo.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace grpcCLien1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var input = new HelloRequest { Name = "tuan"};
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(input);
+
+            //Console.WriteLine(reply.Message);
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var customerClient = new Customer.CustomerClient(channel);
+
+
+            var clientRequest = new CustomerLookupModel { UserId = 2 };
+            var customer = await customerClient.GetCustomerInfoAsync(clientRequest);
+            Console.WriteLine($"{customer.FistName}{customer.LastName}");
+            Console.WriteLine("New Customer List");
+            Console.WriteLine();
+
+            using (var call = customerClient.GetNewCustomers(new NewCustomerRetquest()))
+            {
+                while (await call.ResponseStream.MoveNext())
+                {
+                    var currentCustomer = call.ResponseStream.Current;
+                    Console.WriteLine($"{ currentCustomer.FistName}{currentCustomer.LastName} : {currentCustomer.EmailAddrees}");
+                }
+            }
+                Console.ReadLine();
+        }
+    }
+}
